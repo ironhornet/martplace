2562bd1352cf98fc0643d0aa8ea2e90839317463
ironhornet <tarmyrka@gmail.com>
2020-09-06 20:42:48 +0300

    added header, search, adventages, almost product slider

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+// $(function(){
+//       $('.product-slider__inner').slick({
+//         dots: true,
+//         arrows: false,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+
+//       });
+
+ 
+//         $(".product__item-rate-star").rateYo({
+//           rating: 3.6
+//         });
+       
+      
+
+// });
