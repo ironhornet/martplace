5737a418634913e639cc5f44bd2620ddf345d770
ironhornet <tarmyrka@gmail.com>
2020-09-09 22:07:51 +0300

    finished product slider , doing products

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,22 @@
-// $(function(){
-//       $('.product-slider__inner').slick({
-//         dots: true,
-//         arrows: false,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
+$(function(){
+      $('.product-slider__inner').slick({
+        prevArrow: ('.prev-arrow'),
+        nextArrow: ('.next-arrow'),
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
 
-//       });
+      });
 
  
-//         $(".product__item-rate-star").rateYo({
-//           rating: 3.6
-//         });
+        $(".product__item-rate-ct-star").rateYo({
+          rating: 4.5,
+          eadOnly: true,
+          starWidth: "15px",
+          readOnly: true
+        });
        
       
 
-// });
+});
